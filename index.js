1169541e35abbb1cb7cf587dfa791269b6cc0cd6
Fabien COLAS <fabjas.colas@gmail.com>
2019-11-28 16:54:57 +0900

    Fill the Products tab && add getAllProducts root

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express')
-const bodyParser = require('body-parser')
-const store = require('./store')
 const app = express()
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.listen(7555, () => {
-    console.log('Server running on http://localhost:7555')
+
+// open the database
+let db = new sqlite3.Database('./db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the CAU BURGER database.');
+});
+
+app.get('/getAllProducts', function (req, res) {
+    db.all(`SELECT type_id, name price FROM Products`, (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            res.json({
+                "message":"success",
+                "data":rows
+            })
+        }
+        //console.log(row.name + "\t\t" + row.price);
+    });
+})
+
+app.listen(8001, function () {
+    console.log('Server listening on port 8001!')
 })

4540fa720d64ff42e0ec90c900d82ae907f7cbbc
Fabien COLAS <fabjas.colas@gmail.com>
2019-12-01 00:16:21 +0900

    Fix CreateOrder

--- index.js
+++ index.js
@@ -36,25 +36,47 @@ let db = new sqlite3.Database('./db', sqlite3.OPEN_READWRITE, (err) => {
     });
 });*/
 
-app.post("/api/addProductToOrder", (req, res, next) => {
+app.post("/api/createOrder", (req, res, next) => {
+    var OrderID;
     var data = {
-        order_id: req.body.order_id,
-        product_id: req.body.product_id,
+        user: req.body.user,
+        booth_id: req.body.booth_id,
+        takeout: req.body.takeout,
+        total_price: req.body.totalprice,
+        products: req.body.products
     }
-    db.run(
-        "INSERT INTO OrderContent (order_id, product_id) values (?, ?)",
-        [data.order_id, data.product_id],
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": err})
-                return;
-            }
+
+    if (data.takeout == "true") {
+        data.takeout = 1;
+    } else {
+        data.takeout = 0;
+    }
+    if (data.user == null) {
+        data.user = 0;
+    }
+    db.run(`INSERT INTO Orders (booth_id, confirmed, total_price, user, takeout) VALUES (?, '0', ?, ?, ?)`,[data.booth_id, data.total_price, data.user, data.takeout], function (err, result) {
+        if (err) {
+            console.error(err.message);
+        } else {
+            OrderID = this.lastID;
             res.json({
-                message: "success",
-                data: data,
-                changes: this.changes
+                "message":"success",
+                "id":this.lastID
             })
-        });
+        }
+    });
+    var i = 0;
+    while (data.products[i]) {
+        db.run(
+            "INSERT INTO OrderContent (order_id, product_id) values (?, ?)",
+            [OrderID, data.products[i].id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({"error": err})
+                }
+            });
+        i++;
+    }
 })
 
 app.get('/api/getAllProducts', function (req, res) {
@@ -83,12 +105,27 @@ app.get('/api/getAllProductType', function (req, res) {
     });
 })
 
+app.get('/api/getAllCommandsContent', function (req, res) {
+    db.all(`SELECT * FROM OrderContent`, (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            res.json({
+                "message":"success",
+                "data":rows
+            })
+        }
+    });
+})
 
-app.post('/api/createOrder', function (req, res) {
+app.post('/api/confirmOrder/:orderId', function (req, res) {
     var data = {
-        booth_id: req.body.name,
+        booth_id: req.body.booth_id,
+        user: req.body.user,
+        total_price: req.body.total_price,
+        takeout: req.body.takeout
     }
-    db.run(`INSERT INTO Orders (booth_id, confirmed) VALUES (?, '0')`[data.booth_id], function (err, result) {
+    db.run(`UPDATE Orders set confirmed = '1'WHERE id = ?`,[req.params.orderId], function (err, result) {
         if (err) {
             console.error(err.message);
         } else {
@@ -99,6 +136,22 @@ app.post('/api/createOrder', function (req, res) {
         }
     });
 })
+
+/*app.post('/api/createOrder', function (req, res) {
+    var data = {
+        booth_id: req.body.name,
+    }
+    db.run(`INSERT INTO Orders (booth_id, confirmed) VALUES (?, '0')`,[data.booth_id], function (err, result) {
+        if (err) {
+            console.error(err.message);
+        } else {
+            res.json({
+                "message":"success",
+                "id":this.lastID
+            })
+        }
+    });
+})*/
 
 app.listen(8001, function () {
     console.log('Server listening on port 8001!')

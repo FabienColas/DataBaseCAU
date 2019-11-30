1cd9a6e1093fd9f3adadeda3cb10d8321cb3f100
Fabien COLAS <fabjas.colas@gmail.com>
2019-11-30 17:36:41 +0900

    Add cors package

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser');
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 

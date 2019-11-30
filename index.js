c97355ee918d45343294070fa7c5007fb07642b5
Fabien COLAS <fabjas.colas@gmail.com>
2019-11-30 17:44:18 +0900

    fix cors issue

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const bodyParser = require('body-parser');
 
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
